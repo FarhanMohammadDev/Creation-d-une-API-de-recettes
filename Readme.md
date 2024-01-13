fac688458e4d234801d581c5f8ccc55e2b6f424e
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-01-13 18:41:34 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# API de Gestion de Recettes - FlavorWorld
+
+## Description
+Bienvenue dans l'API de gestion de recettes pour le 'Livre de Recettes en Ligne' de FlavorWorld. Cette API, construite avec Express.js et MongoDB, offre des fonctionnalités complètes pour explorer, ajouter, modifier et supprimer des recettes.
+
+## Fonctionnalités Principales
+- Exploration des recettes.
+- Obtention de détails complets sur chaque recette.
+- Ajout de nouvelles recettes.
+- Modification des recettes existantes.
+- Suppression de recettes inappropriées.
+
+## Technologie Utilisée
+- Express.js pour la gestion des requêtes HTTP.
+- MongoDB pour le stockage efficace et sécurisé des données.
+
+## Installation
+1. Clonez le repo : `git clone `
+2. Installez les dépendances : `npm install`
+
+## Configuration
+- Configurez la connexion à MongoDB dans le fichier `configs/db.js`.
+- Assurez-vous d'ajuster les paramètres de sécurité et d'autorisation selon vos besoins.
+
+## Utilisation
+1. Lancez l'API : `npm start`
+2. Explorez les différentes fonctionnalités via les endpoints définis dans la documentation.
+
+
---
+++ controllers/recipeController.js
@@ -0,0 +1,77 @@
+const asyncHandler = require('express-async-handler')
+// npm i joi 
+const {Recipe,validateCreateRecipe, validateUpdateRecipe} = require("../models/Recipe");
+
+
+const recipe_index_get = asyncHandler(async (req, res) => {
+    const recipesList = await Recipe.find();
+    res.status(200).json(recipesList);
+})
+
+const recipe_show_get = asyncHandler(async (req, res) => {
+    const recipe = await Recipe.findById(req.params.id);
+    if (recipe) {
+      res.status(200).json(recipe)
+    } else {
+      res.status(404).json({message:"recipe not found"})
+    }
+})
+
+const recipe_store_post = asyncHandler(async (req,res)=>{
+    console.log(req.body);
+    const {error} = validateCreateRecipe(req.body);
+    if (error) {
+     return res.status(400).json({message : error.details[0].message})
+    }
+    const recipe = new Recipe( {
+      title: req.body.title,
+      category: req.body.category,
+      author:req.body.author,
+      origin: req.body.origin,
+      ingredients: req.body.ingredients,
+      steps: req.body.steps,
+      image: req.body.image
+    });
+      
+    const result = await recipe.save();
+    res.status(201).json(result);
+})
+
+const recipe_update_put = asyncHandler(async (req,res)=>{
+    const {error} = validateUpdateRecipe(req.body);
+    if (error) {
+      return res.status(400).json({message : error.details[0].message})
+    } 
+    
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id,{
+      $set:{
+          title:req.body.title,
+          category: req.body.category,
+          author: req.body.author,
+          origin: req.body.origin,
+          ingredients: req.body.ingredients,
+          steps:req.body.steps,
+          image: req.body.image
+      }
+    },{new:true})
+    res.status(200).json(recipe)
+    
+})
+
+const recipe_destroy_delete =asyncHandler(async (req,res)=>{
+    const recipe = await Recipe.findById(req.params.id);
+    if (recipe) {
+      await Recipe.findByIdAndDelete(req.params.id)
+      return res.status(200).json({message:"recipe has been deleted successfuly "})
+    } else {
+      res.status(404).json({message:"recipe not found"})
+    }
+})
+
+module.exports = {
+    recipe_index_get,
+    recipe_store_post,
+    recipe_show_get,
+    recipe_update_put,
+    recipe_destroy_delete
+};
